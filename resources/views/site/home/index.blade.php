a525c2d885a7e0528d6358a960029b3c9808fc36
williamguilhermesouza <williamguilhermesouza@gmail.com>
2021-11-12 12:32:24 -0300

    working with adminlte

--- resources/views/site/home/index.blade.php
+++ resources/views/site/home/index.blade.php
@@ -1,19 +1,12 @@
 @extends('adminlte::page')
 
-@section('title', 'Dashboard')
+@section('title', 'Home')
 
 @section('content_header')
-    <h1>Dashboard</h1>
+    <h1>Home</h1>
 @stop
 
 @section('content')
     <p>Welcome to this beautiful admin panel.</p>
 @stop
 
-@section('css')
-    <link rel="stylesheet" href="/css/admin_custom.css">
-@stop
-
-@section('js')
-    <script> console.log('Hi!'); </script>
-@stop
