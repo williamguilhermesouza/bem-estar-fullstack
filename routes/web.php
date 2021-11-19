113e13379db665644f1448486374c304a51f3c72
williamguilhermesouza <williamguilhermesouza@gmail.com>
2021-11-19 09:07:04 -0300

    organized routes

--- routes/web.php
+++ routes/web.php
@@ -18,23 +18,28 @@ use App\Http\Controllers\Admin\UserController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::prefix('/admin')->group(function() {
-    Route::get('/', HomeController::class)->name('admin.home');
-    Route::get('/agenda', [AgendaController::class, 'index'])->name('admin.agenda');
-    Route::get('/atendimentos', [AttendanceController::class, 'index'])->name('admin.attendance');
-    Route::get('/movimentacoes', [MovementController::class, 'index'])->name('admin.movement');
-    Route::get('/pacientes', [PatientController::class, 'index'])->name('admin.patient');
-    Route::get('/pacientes/novo', [PatientController::class, 'create'])->name('admin.patient.new');
-    Route::post('/pacientes/salvar', [PatientController::class, 'store'])->name('admin.patient.save');
-    Route::get('/pacientes/mostrar/{id}', [PatientController::class, 'show'])->name('admin.patient.show');
-    Route::get('/pacientes/editar/{id}', [PatientController::class, 'edit'])->name('admin.patient.edit');
-    Route::post('/pacientes/atualizar', [PatientController::class, 'update'])->name('admin.patient.update');
-    Route::get('/pacientes/deletar/{id}', [PatientController::class, 'destroy'])->name('admin.patient.destroy');
-    Route::get('/usuarios', [UserController::class, 'index'])->name('admin.user');
-    Route::get('/usuarios/novo', [UserController::class, 'create'])->name('admin.user.new');
-    Route::post('/usuarios/salvar', [UserController::class, 'store'])->name('admin.user.save');
-    Route::get('/usuarios/deletar/{id}', [UserController::class, 'destroy'])->name('admin.user.destroy');
+Route::prefix('/admin')->name('admin.')->group(function() {
 
+    Route::get('/', HomeController::class)->name('home');
+    Route::get('/agenda', [AgendaController::class, 'index'])->name('agenda');
+    Route::get('/atendimentos', [AttendanceController::class, 'index'])->name('attendance');
+    Route::get('/movimentacoes', [MovementController::class, 'index'])->name('movement');
+
+    Route::prefix('/pacientes')->name('patient.')->group(function() {
+        Route::get('', [PatientController::class, 'index'])->name('patient');
+        Route::get('/novo', [PatientController::class, 'create'])->name('new');
+        Route::post('/salvar', [PatientController::class, 'store'])->name('save');
+        Route::get('/mostrar/{id}', [PatientController::class, 'show'])->name('show');
+        Route::get('/editar/{id}', [PatientController::class, 'edit'])->name('edit');
+        Route::post('/atualizar', [PatientController::class, 'update'])->name('update');
+        Route::get('/deletar/{id}', [PatientController::class, 'destroy'])->name('destroy');
+    });
+    Route::prefix('/usuarios')->name('user.')->group(function() {
+        Route::get('', [UserController::class, 'index'])->name('');
+        Route::get('/novo', [UserController::class, 'create'])->name('new');
+        Route::post('/salvar', [UserController::class, 'store'])->name('save');
+        Route::get('/deletar/{id}', [UserController::class, 'destroy'])->name('destroy');
+    });
 });
 
 Route::redirect( '/', '/login' );
